bd9504ce1b56a9d1587db2c1a6bab14687df0f67
ted80-Laptop <meatkroket@hotmail.com>
2014-05-20 10:24:33 +0200

    Removed debug text in generator

--- Assets/script/gen/GeneratorTest.cs
+++ Assets/script/gen/GeneratorTest.cs
@@ -12,8 +12,7 @@ public class GeneratorTest : IGenerator {
 		
 		for (int i = 0; i < size; i++) {
 			for (int j = 0; j < size; j++) {
-				int t = (int) (Mathf.PerlinNoise((float)i / 7f, (float)j / 7f) * 6) - 1;
-				Debug.Log(Mathf.PerlinNoise((float)i / 7f, (float)j / 7f) * 3);
+				int t = (int) (Mathf.PerlinNoise((float)i / 7f, (float)j / 7f) * 5) - 1;
 				t = t < 0 ? 0 : t > 2 ? 2 : t;
 				tiles [i, j] = t;
 			}
