80f71852be10c0e20c5a1a7c9383300e01a84ba7
Kit van de Bunt <kitlvdbunt@hotmail.com>
2014-05-21 23:30:56 +0200

    multi select

--- Assets/script/Main.cs
+++ Assets/script/Main.cs
@@ -1,18 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Main : MonoBehaviour {
 	[SerializeField]
 	private Camera cam;
+	[SerializeField]
+	private GameObject multiSelectArea;
 	
 	private bool mouseDown = false;
 	private Vector2 oldPos;
 	private int[] selectedIDs;
 	private int[] location;
 	private bool multiselect;
+	private Vector3 startMousePos;
+	private VecInt startTilePos;
 
 	private void Start(){
 		GameInput.init(this);
+		multiSelectArea.SetActive(false);
 	}
 
 	private void Update(){
@@ -21,29 +27,62 @@ public class Main : MonoBehaviour {
 	
 	public void UpdateSelect(){
 		Vector2? TilePosN = IsoMath.getMouseTilePosition();
-		if(TilePosN!= null){
-			Vector2 TilePos = (Vector2)TilePosN;
-			//print ("tile: " + TilePos + "\n");
-			if(Input.GetMouseButtonDown(0)&&Input.GetKeyDown(KeyCode.RightControl)){
-				multiselect = true;
-			}else if(Input.GetMouseButtonDown(0)){
-				GameObject selected = LevelData.GroundVehicles [(int)Mathf.Floor (TilePos.x), (int)Mathf.Floor (TilePos.y)];
-				if(selected != null){
-					selectedIDs = new int[]{selected.GetInstanceID()};
-					Debug.Log("selected: "+selectedIDs.Length);
-					EventManager.CallOnSelect(selectedIDs);
-				}else{
-					selectedIDs = new int[]{};
-					Debug.Log("not selected: "+selectedIDs.Length);
-					EventManager.CallOnSelect(selectedIDs);
+		if (multiselect) {
+			Vector2 currentMousePos = IsoMath.getMouseWorldPosition();
+			float areaWidth = currentMousePos.x-startMousePos.x;
+			float areaHeight = currentMousePos.y-startMousePos.y;
+			//Debug.Log("area("+areaWidth+","+areaHeight+")");
+			multiSelectArea.transform.localScale = new Vector3(areaWidth,areaHeight,1);
+			if (Input.GetMouseButtonUp (0)) {
+				VecInt[] selectedArea = IsoMath.Area(startTilePos,areaWidth,areaHeight,new Rect?(new Rect(0,0,LevelData.width,LevelData.height)));
+				List<int> tempSelectedIds = new List<int>(); 
+				for(int i = 0;i < selectedArea.Length;i++){
+					//Debug.Log("selected: ("+selectedArea[i].x+","+selectedArea[i].y+")");
+					LevelData.LoadedGroundTiles[selectedArea[i].x,selectedArea[i].y].GetComponent<SpriteRenderer>().color = new Color(0,0,1,1);
+					GameObject selected = LevelData.GroundVehicles [(int)Mathf.Floor (selectedArea[i].x), (int)Mathf.Floor (selectedArea[i].y)];
+					if(selected != null){
+						tempSelectedIds.Add(selected.GetInstanceID());
+					}
 				}
-			}else if(Input.GetMouseButtonDown(1)){
-				print ("mouse1");
-				if(selectedIDs.Length > 0){
-					PathFind.FindPath (
-					new int[]{(int)Mathf.Floor (TilePos.x),(int)Mathf.Floor (TilePos.y)}
-					, new int[]{10,10}
-					, LevelData.GroundVehicles);
+				selectedIDs = tempSelectedIds.ToArray();
+				Debug.Log("selected: "+selectedIDs.Length);
+				EventManager.CallOnSelect(selectedIDs);
+				multiselect = false;
+				multiSelectArea.SetActive(false);
+			}
+		}else{
+			if(TilePosN!= null){
+				Vector2 TilePos = (Vector2)TilePosN;
+				//print ("tile: " + TilePos + "\n");
+				//multi select
+				if(Input.GetMouseButtonDown(0)&&(Input.GetKey(KeyCode.RightControl)||Input.GetKey(KeyCode.LeftControl))){
+					multiselect = true;
+					multiSelectArea.SetActive(true);
+					startMousePos = IsoMath.getMouseWorldPosition3D();
+					multiSelectArea.transform.position = startMousePos;
+					multiSelectArea.transform.localScale = Vector3.zero;
+					startTilePos = new VecInt((int)TilePos.x,(int)TilePos.y);
+					Debug.Log("Multiselect: "+startTilePos);//+selectedIDs.Length);
+				}else if(Input.GetMouseButtonDown(0)){
+				//single select
+					GameObject selected = LevelData.GroundVehicles [(int)Mathf.Floor (TilePos.x), (int)Mathf.Floor (TilePos.y)];
+					if(selected != null){
+						selectedIDs = new int[]{selected.GetInstanceID()};
+						Debug.Log("selected: "+selectedIDs.Length);
+						EventManager.CallOnSelect(selectedIDs);
+					}else{
+						selectedIDs = new int[]{};
+						Debug.Log("not selected: "+selectedIDs.Length);
+						EventManager.CallOnSelect(selectedIDs);
+					}
+				}else if(Input.GetMouseButtonDown(1)){
+					print ("mouse1");
+					if(selectedIDs.Length > 0){
+						PathFind.FindPath (
+						new int[]{(int)Mathf.Floor (TilePos.x),(int)Mathf.Floor (TilePos.y)}
+						, new int[]{10,10}
+						, LevelData.GroundVehicles);
+					}
 				}
 			}
 		}
--- Assets/script/utils/IsoMath.cs
+++ Assets/script/utils/IsoMath.cs
@@ -1,10 +1,13 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class IsoMath : MonoBehaviour {
 
 	private static float tileW = 0.90f;
 	private static float tileH = 0.50f;
+	private static int i;
+	private static int j;
 
 	public static Vector2 tileToWorld(int tx, int ty){
 		return new Vector2((ty * tileW/2) + (tx * tileW/2), -((tx * tileH/2) - (ty * tileH/2)));
@@ -29,6 +32,13 @@ public class IsoMath : MonoBehaviour {
 		Vector2 mousePos2D = new Vector2(mouseRay.origin.x,mouseRay.origin.y);
 		return mousePos2D;
 	}
+
+	public static Vector3 getMouseWorldPosition3D(){
+		Vector2 returnValue;
+		Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+		Vector3 mousePosition = new Vector3(mouseRay.origin.x,mouseRay.origin.y,10);
+		return mousePosition;
+	}
 	
 	public static Vector2? getMouseTilePosition(){
 		Vector2 mousePos2D = getMouseWorldPosition();
@@ -44,10 +54,64 @@ public class IsoMath : MonoBehaviour {
 		}
 	}
 
-	public static VecInt[] Area(VecInt a,VecInt b){
-		print ("A ("+a.x+","+ a.y+")");
-		print ("b ("+b.x+","+ b.y+")");
-		return new VecInt[]{};
+	public static VecInt[] Area(VecInt a,float width,float height,Rect? rect){
+		List<VecInt> tempList = new List<VecInt> ();
+		List<VecInt> tempList2 = new List<VecInt> ();
+		int wSteps = (int)((width * 100) / LevelData.width);
+		int hSteps = (int)((height * 100) / LevelData.height);
+		print ("A (" + a.x + "," + a.y + ") width: "+wSteps+" height: "+hSteps);
+		int up = 1;
+		int right = 1;
+		//wSteps += 1;
+		//hSteps += 1;
+		if (wSteps < 0) {
+			wSteps*=-1;
+			right = -1;
+		}
+		if (hSteps < 0) {
+			hSteps*=-1;
+			up = -1;
+		}
+		//Debug.Log("hSteps: "+hSteps);
+		for (j = 0; j < hSteps; j+=1) {
+			//Debug.Log("========================== j: "+j);
+			for (i = 0; i < wSteps; i+=1) {
+				//Debug.Log("========================== i: "+i);
+				int x = a.x +(i*right)-(j*up);
+				int y = a.y +(i*right)+(j*up);
+				tempList.Add(new VecInt(x,y));
+				if((i != wSteps-1)&&(j != hSteps-1)){
+					//Debug.Log("========================== end: ");
+					if(up == -1 && right == -1){
+						y -=1;
+					}else if(up==-1){
+						x +=1;
+					}else if(right==-1){
+						x -=1;
+					}else{
+						y +=1;
+					}
+					tempList.Add(new VecInt(x,y));
+					
+				}
+			}
+		}
+		//check if in rect
+		if(rect!=null){
+			print ("null");
+			Rect rec = (Rect)rect;
+			for (i =0;i<tempList.Count;i++){
+				//print ("(" + tempList[i].x + "," + tempList[i].y + ")");
+				if(tempList[i].x>rec.width-1||tempList[i].x<0){
+				
+				}else if(tempList[i].y>rec.height-1||tempList[i].y<0){
+				
+				}else{
+					tempList2.Add(tempList[i]);
+				}
+			}
+		}
+		return tempList2.ToArray ();
 	}
 }
 
