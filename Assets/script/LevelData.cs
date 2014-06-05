897073b00e5b544f3c1352acdda439987c0486ab
ted80-Laptop <meatkroket@hotmail.com>
2014-06-05 12:23:39 +0200

    tiles/objects z-position fix

--- Assets/script/LevelData.cs
+++ Assets/script/LevelData.cs
@@ -24,7 +24,7 @@ public class LevelData : MonoBehaviour{
 	public static int[,] tileData;
 	public static int[,] objectData;
 	private static bool[,] collsionData;
-	public static List<IBuilding> buildingList;
+	public static List<Building> buildingList;
 	
 	private static GameObject levelHolder;
 	
@@ -61,17 +61,17 @@ public class LevelData : MonoBehaviour{
 		height = tileData.GetLength(1);
 		collsionData = new bool[size,size];
 		objectData = RandomTestData (size,size,new int[]{0,0,0,0,0,0,1});
-		buildingList = new List<IBuilding>();
+		buildingList = new List<Building>();
 
 		BuildTiles (tileData);
 		BuildObjects(objectData);
 
 		for(int j = 0; j < 16; j++) {
-			constructBuilding(5, 6 + j * 2, 0, 2);
-			constructBuilding(9, 6 + j * 2, 1, 2);
-			constructBuilding(13, 6 + j * 2, 2, 2);
-			constructBuilding(15, 6 + j * 2, 3, 1);
-			constructBuilding(0, j, 3, 1);
+			constructBuilding(5 , 6 + j * 2     , 0, 2);
+			constructBuilding(9 , 6 + j * 2     , 1, 2);
+			constructBuilding(13, 6 + j * 2     , 2, 2);
+			constructBuilding(15, 6 + j * 2     , 3, 1);
+			constructBuilding(15, 6 + j * 2 + 1 , 3, 1);
 		}
 	}
 
@@ -137,10 +137,10 @@ public class LevelData : MonoBehaviour{
 			}
 		}
 
-		Vector2 pos = IsoMath.tileToWorld(x + (size / 2), y + (size / 2));
-		GameObject building = (GameObject)GameObject.Instantiate (staticBuildings[id], new Vector3 (pos.x, pos.y, (pos.x - 1) * pos.y / 40f + 5f), new Quaternion());
+		Vector2 pos = IsoMath.tileToWorld(x, y);
+		GameObject building = (GameObject)GameObject.Instantiate (staticBuildings[id], new Vector3 (pos.x, pos.y, 0f), new Quaternion());
 		building.transform.parent = levelHolder.transform;
-		//IBuilding s = (IBuilding)building.GetComponent(typeof(IBuilding));  <--- is now done in the building start() func.
+		//IBuilding s = (IBuilding)building.GetComponent(typeof(Building));  <--- is now done in the building start() func.
 		//buildingList.Add(s);
 		//calculateEnegy();
 		return true;
@@ -149,7 +149,8 @@ public class LevelData : MonoBehaviour{
 	public static Vector3 addSizeToPosition(Vector3 vec, int width, int height) {
 		float px = vec.x - ((width - 1f) * 0.5f);
 		float py = vec.y + ((height - width) * 0.25f);
-		return new Vector3(px, py, px * py / 40f + 5f);
+		Vector2 tilepos = IsoMath.worldToTile(vec.x, vec.y);
+		return new Vector3(px, py, (tilepos.y + (size - tilepos.x)) / 2.5f + 2f);
 	}
 	
 	private static void BuildObjects (int[,] data) {
@@ -161,7 +162,7 @@ public class LevelData : MonoBehaviour{
 				if(objectID != 0){
 					objectID -= 1;
 					Vector2 pos = IsoMath.tileToWorld(w,h);
-					GameObject tile = (GameObject)GameObject.Instantiate (staticObjects[objectID], new Vector3 (pos.x, pos.y, pos.x * pos.y / 40f + 5f), new Quaternion ());
+					GameObject tile = (GameObject)GameObject.Instantiate (staticObjects[objectID], new Vector3 (pos.x, pos.y, (h + (size - w)) / 2.5f + 2f), new Quaternion ());
 					tile.transform.parent = levelHolder.transform;
 					GroundVehicles[w,h] = new MapObject(tile,new VecInt(w,h));
 					collsionData[w,h] = true;
@@ -174,8 +175,8 @@ public class LevelData : MonoBehaviour{
 		LoadedGroundTiles = new GameObject[width,height];
 		for (h = 0; h < height; h++) {
 			for (w = 0; w < width; w++) {
-				Vector2 pos = IsoMath.tileToWorld(w,h);
-				GameObject tile = (GameObject)GameObject.Instantiate (staticTiles[data[w,h]], new Vector3 (pos.x, pos.y, pos.x * pos.y / 40f + 10f), new Quaternion ());
+				Vector2 pos = IsoMath.tileToWorld(w,h); 
+				GameObject tile = (GameObject)GameObject.Instantiate (staticTiles[data[w,h]], new Vector3 (pos.x, pos.y, (h + (size - w)) / 2.5f + 10f), new Quaternion ());
 				tile.transform.parent = levelHolder.transform;
 				LoadedGroundTiles[w,h] = (GameObject)tile;
 			}
--- Assets/script/entities/buildings/BuildingPower.cs
+++ Assets/script/entities/buildings/BuildingPower.cs
@@ -1,69 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class BuildingPower : MonoBehaviour, IBuilding {
-	[SerializeField]
-	private int buyAmount;
-	[SerializeField]
-	private int sellAmount;
-	[SerializeField]
-	private int energy;
-	[SerializeField]
-	private int maxHealth;
-	private int health;
+public class BuildingPower : Building {
+	public static int width = 2;
+	public static int height = 3;
 	
-	[SerializeField]
-	private int buildingWidth;
-	[SerializeField]
-	private int buildingHeight;
-	
-	private SpriteRenderer spriterenderer;
-
 	void Start () {
+		buyAmount = 1000;
+		sellAmount = 400;
+		energy = 500;
+		health = 100;
+		
 		health = maxHealth;
+		buildingWidth = width;
+		buildingHeight = height;
 		spriterenderer = GetComponent<SpriteRenderer>();
+		onEnergyStateChange(LevelData.ENERGY);
 		LevelData.buildingList.Add(this);
 		LevelData.calculateEnegy();
 		transform.position = LevelData.addSizeToPosition(transform.position, getBuildingWidth(), getBuildingHeight());	
 	}
 	
 	void Update () {
-		
-	}
-	
-	public int getHealth() {
-		return health;
-	}
-	
-	public int getEnergyUsage() {
-		return energy;
-	}
-
-	public bool damage(int amount) {
-		health -= amount;
-		if(health < 0)
-		{
-			return true;
-		}
-		return false;
-	}
-	
-	public void onEnergyStateChange(bool state) {
-	}
-
-	public int getBuyAmount() {
-		return buyAmount;
-	}
-	
-	public int getSellAmount() {
-		return sellAmount;
-	}
-	
-	public int getBuildingWidth() {
-		return buildingWidth;
-	}
-	
-	public int getBuildingHeight() {
-		return buildingHeight;
 	}
 }
